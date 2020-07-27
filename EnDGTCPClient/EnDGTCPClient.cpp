4bfe1c06f83b14ecd51dff0d6a779431f0ab9d9b
unknown <zhuominmark.hu@aptiv.com>
2020-07-26 21:03:52 -0400

    EnDGTCPClient

---
+++ EnDGTCPClient/EnDGTCPClient.cpp
@@ -0,0 +1,106 @@
+// EnDGTCPClient.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+int sd;
+struct sockaddr_in localAddr, servAddr;
+int server_port = 13400;
+char *sendbuffer="How are you?"; 
+char recvbuffer[100];
+char stIPAddress[20];
+const char *IPADDRESS="C:/IPADDRESS.txt";
+
+int WinsockInitialize(){
+    int err;
+	WORD wVersionReg;
+	WSADATA wsaData;
+
+	wVersionReg = MAKEWORD(1, 1);
+	err = WSAStartup(wVersionReg, &wsaData);
+	if (err != 0)
+       return (1);
+    return (0);
+}
+
+int main(int argc, char* argv[])
+{
+	int ret, rc;
+	printf("Hello World!\n");
+  
+	FILE * pFile=NULL;
+    pFile = fopen (IPADDRESS , "r");
+    if (pFile !=NULL) {
+        fgets (stIPAddress , 19 , pFile);
+        fclose (pFile);
+	}
+	else{
+		printf("can't open the file!\n");
+      //  strcpy(stIPAddress,"169.254.28.229");
+	} 
+	strcpy(stIPAddress,"169.254.144.82");
+	ret=WinsockInitialize();
+	if (ret==1){
+  	    printf("WinsockInitializeError\n");
+        return 1;
+    }
+  	printf("WinsockInitializeSuccess\n");
+
+   /* create socket */
+    sd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(sd == INVALID_SOCKET){
+       printf("socket failed with error: %ld\n", WSAGetLastError());
+       WSACleanup();
+       return 1;
+	}
+	while(1){
+
+    servAddr.sin_family = AF_INET;
+  //  servAddr.sin_addr.s_addr = inet_addr("169.254.136.120");
+
+//    servAddr.sin_addr.s_addr = inet_addr("169.254.28.115");
+ //   servAddr.sin_addr.s_addr = inet_addr("169.254.28.229");
+    servAddr.sin_addr.s_addr = inet_addr(stIPAddress);
+ //   servAddr.sin_addr.s_addr = inet_addr("169.254.41.13");
+//	    servAddr.sin_addr.s_addr = inet_addr("192.168.2.218");
+//	servAddr.sin_addr.s_addr = inet_addr(???);
+
+  //    servAddr.sin_addr.s_addr = inet_addr(stIPAddress);
+    servAddr.sin_port = htons(server_port);
+    rc = connect(sd, (struct sockaddr *) &servAddr, sizeof(servAddr));
+    if (rc==SOCKET_ERROR ){
+		 wprintf(L"socket failed with error: %ld\n", WSAGetLastError());
+         closesocket(sd);
+         WSACleanup();
+         return 0;
+    }
+		while (1){
+		// send how are u
+			rc = send(sd, sendbuffer, (int)strlen(sendbuffer), 0);
+			if (rc == SOCKET_ERROR) {
+				printf("send failed with error: %d\n", WSAGetLastError());
+				closesocket(sd);
+				WSACleanup();
+				return 1;
+			}
+			printf("Bytes Sent: %ld\n", rc);
+		// receive message
+			rc = recv(sd, recvbuffer, 100, 0);
+
+			if ( rc > 0 )
+				  printf("Bytes received: %d, %s \n", rc, recvbuffer); 
+			else{
+				printf("receive failed with error: %d\n", WSAGetLastError());
+				closesocket(sd);
+				WSACleanup();
+				break;
+			}	 
+		}
+}
+	    // cleanup
+    closesocket(sd);
+    WSACleanup();
+	return 0;
+}
+
